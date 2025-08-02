d8cc2c7f875cde0194e90801c7d4a3b9b061e8da
Math-Fauch <faucher.mathieu@gmail.com>
2025-08-02 14:53:40 -0400

    Add fns

--- dist/main.js
+++ dist/main.js
@@ -2,5 +2,12 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.main = main;
 function main() {
-    console.log("Hello, world!");
+    console.log(calculateSum(1, 2));
+    console.log(calculateSub(2, 1));
+}
+function calculateSum(a, b) {
+    return a + b;
+}
+function calculateSub(a, b) {
+    return a - b;
 }
--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,15 @@
-export function main() {
-    console.log("Hello, world!")
+function main() {
+  console.log(calculateSum(1, 2));
+  console.log(calculateSub(5, 1));
 }
+
+function calculateSum(a: number, b: number): number {
+  return a + b;
+}
+
+function calculateSub(a: number, b: number): number {
+  return a - b;
+}
+
+main();
+
