c24160d594b7839d126c9dc79be45e88da7b199e
Math-Fauch <faucher.mathieu@gmail.com>
2025-08-02 15:05:18 -0400

    Add octokit and dotenv

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,9 @@
+import dotenv from 'dotenv';
+import { Octokit } from 'octokit';
+
+require('dotenv').config();
+
+
 function main() {
   console.log(calculateSum(1, 2));
   console.log(calculateSub(5, 1));
@@ -9,6 +15,10 @@ function calculateSum(a: number, b: number): number {
 
 function calculateSub(a: number, b: number): number {
   return a - b;
+}
+
+function getCommitNumber() {
+    const gh-key = new Octokit({auth: process.env.GITHUB_API_KEY})
 }
 
 main();
