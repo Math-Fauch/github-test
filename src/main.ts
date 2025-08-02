0c238db7a24be891f216880ee8f448b261b31255
Math-Fauch <faucher.mathieu@gmail.com>
2025-08-02 16:26:39 -0400

    Working commits by dates

--- src/main.ts
+++ src/main.ts
@@ -1,38 +1,51 @@
-import { Octokit } from 'octokit';
-import 'dotenv/config';
+import { Octokit } from "octokit";
+import "dotenv/config";
 
-
-function main() {
-  getCommitNumber()
+async function main() {
+  const oneDay = 1000 * 60 * 60 * 24;
+  const numberOfDays = 1.2;
+  let commitNum: number = await getCommitNumberForDate(
+    0,
+    Date.now() - numberOfDays * oneDay
+  );
+  console.log(commitNum);
 }
 
-function calculateSum(a: number, b: number): number {
-  return a + b;
+async function getCommitNumberForDate(
+  startDate: number,
+  endDate: number
+): Promise<number> {
+  const commits = await getCommits("Math-Fauch", "github-test");
+
+  let commitCount = 0;
+  for (let i: number = 0; i < commits.data.length; i++) {
+    let commitDate: number = Date.parse(commits.data[i].commit.author.date);
+
+    if (endDate < commitDate) {
+      continue;
+    }
+    if (startDate > commitDate) {
+      break;
+    }
+    commitCount++;
+  }
+
+  return commitCount;
 }
 
-function calculateSub(a: number, b: number): number {
-  return a - b;
-}
+async function getCommits(owner: string, repo: string): Promise<object> {
+  const octokit = new Octokit({ auth: process.env.GITHUB_API_KEY });
+  const request = "GET /repos/" + owner + "/" + repo + "/commits";
 
-async function getCommitNumber() {
-    const octokit = new Octokit({auth: process.env.GITHUB_API_KEY})
-    const owner = 'Math-Fauch'
-    const repo = 'github-test'
-    const request = "GET /repos/" + owner + "/" + repo + "/commits";
+  const result = await octokit.request(request, {
+    owner: owner,
+    repo: repo,
+    headers: {
+      "X-GitHub-Api-Version": "2022-11-28",
+    },
+  });
 
-    const result = await octokit.request(
-      request,
-      {
-        owner: owner,
-        repo: repo,
-        headers: {
-          "X-GitHub-Api-Version": "2022-11-28",
-        },
-      }
-    );
-
-    console.log(result.data[0])
+  return result;
 }
 
 main();
-
